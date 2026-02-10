3e3975c3e9438dae79b0c6fb48d1bd6afb257cc4
Harshwardhan Deshmane <deshmaneh003@gmail.com>
2026-02-10 13:02:38 +0530

    done

---
+++ 2 Month.Feb/React/gmail-clone/src/App.jsx
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className="text-center bg-danger">HOC</h1>
+
+      <p> Designing and managing NoSQL database for efficient data storage and scalability.</p>
+      <p>Designing and managing NoSQL database for efficient data storage and scalability.</p>
+      <p>Designing and managing NoSQL database for efficient data storage and scalability.</p>
+      <p>Designing and managing NoSQL database for efficient data storage and scalability.</p>
+
+      <ul>
+
+        <li>Home</li>
+        <li> About Us </li>
+        <li> Contact Us  </li>
+          
+      </ul>
+
+    </>
+  )
+}
+
+export default App
+
+
+
+
+    
