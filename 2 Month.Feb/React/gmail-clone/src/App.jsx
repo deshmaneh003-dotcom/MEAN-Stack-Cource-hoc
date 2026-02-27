090a0888b425e12997aa9d574eba3a2f93f84e47
Harshwardhan Deshmane <deshmaneh003@gmail.com>
2026-02-27 19:34:16 +0530

    done

--- 2 Month.Feb/React/gmail-clone/src/App.jsx
+++ 2 Month.Feb/React/gmail-clone/src/App.jsx
@@ -108,6 +108,7 @@ function App() {
                 </svg>
                 compose
               </button>
+              
 
               <ul>
 
@@ -182,11 +183,36 @@ function App() {
 
 
             </div>
-            
+
+
 
             {/* Main Content  */}
             <div className="col-md-10 main-content">
-              
+              {
+             <div className='container'>
+                <div className='row'>
+                  <div className='col-md 3'>
+                    Primary
+                  </div>
+                  <div className='col-md 3'>
+                    Promotions
+                  </div>
+                  <div className='col-md 3'>
+                 
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                        <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
+                      </svg>
+                      Social
+                    
+                  </div>
+                  <div className='col-md 3'>
+                    Updates
+                  </div>
+
+                </div>
+
+              </div> }
+
               <div className="email-list box-1">
                 <div className="email-row d-flex align-item-center border-bottom read">
                   <Form.Check type="checkbox" className="email-checkbox p-2" />
@@ -606,9 +632,70 @@ function App() {
                 </div>
               </div>
               <div className="box-2">
-              <h3> Test</h3>
+                <img
+ 
+/>    
 
-              </div>
+<div>
+  <button className="btn">
+    <img
+      src="https://tse4.mm.bing.net/th/id/OIP.1Op6caaKQT3ue9B2qUdSCQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3"
+      alt="keep"
+      width="40"
+      height="30"
+    />
+  </button>
+</div>
+
+<div>
+  <a
+    href="https://tasks.google.com"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <button className="btn">
+      <img
+        src="https://fonts.gstatic.com/s/i/productlogos/tasks/v10/192px.svg"
+        
+        width="40"
+        height="30"
+      />
+    </button>
+  </a>
+</div>
+<div>
+  <a
+    href="https://contacts.google.com"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <button className="btn">
+      <img
+        src="https://static.vecteezy.com/system/resources/thumbnails/021/286/293/small_2x/doodle-logo-or-icon-contact-people-human-line-art-free-png.png"
+       
+        width="40"
+        height="30"
+        
+      />
+      
+    </button>
+  </a>
+</div>
+
+<div>
+  <button className="btn">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="30"
+      fill="#5f6368"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 1a.5.5 0 0 1 .5.5V7.5H14.5a.5.5 0 0 1 0 1H8.5V14.5a.5.5 0 0 1-1 0V8.5H1.5a.5.5 0 0 1 0-1H7.5V1.5A.5.5 0 0 1 8 1z"/>
+    </svg>
+  </button>
+</div>
+  </div>
             </div>
           </div>
         </div>
